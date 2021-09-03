38d465bb113ab440bf5e09349c71017ba6716582
Travis Woodward <trwoodward@hotmail.com>
2021-09-03 23:50:40 +0100

    Stubbed out conversion operators and core methods

--- RegExStringLib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RegExStringLib
-{
-    public class Class1
-    {
-    }
-}
---
+++ RegExStringLib/RegExString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RegExStringLib
+{
+    public class RegExString
+    {
+        //Internal variables
+        private string currentString;
+
+        //Conversion operators
+        public override string ToString() => currentString;
+        public static implicit operator string(RegExString exString) => exString.currentString;
+
+        //Core methods
+        public static RegExString Matching(RegExString subexpression)
+        {
+            //ToDo - correct implementation
+            RegExString expression = new RegExString();
+            expression.currentString += subexpression;
+            return expression;
+        }
+
+        public RegExString Then(RegExString subexpression)
+        {
+            //ToDo - correct implementation
+            currentString += subexpression;
+            return this;
+        }
+        
+    }
+}
